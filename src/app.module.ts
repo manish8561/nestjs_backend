bac0db771df414d16684c353b808df4119f84d41
manish <manish.sharma@antechindia.com>
2023-05-28 23:22:28 +0530

    adding network in the docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FavoritesModule } from './favorites/favorites.module';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://mongodb:27017/nest'),
+    FavoritesModule,
+    MoviesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/movies/movies.service.ts
@@ -0,0 +1,47 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable, Logger } from '@nestjs/common';
+import { AxiosError } from 'axios';
+import { catchError, firstValueFrom, Observable } from 'rxjs';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Injectable()
+export class MoviesService {
+  private readonly logger = new Logger(MoviesService.name);
+  constructor(private readonly httpService: HttpService) {}
+  create(createMovieDto: CreateMovieDto) {
+    return { data: 'This action adds a new movie' };
+  }
+
+  // async findAll(): Promise<any> {
+  //   const a = await this.httpService.axiosRef.get(
+  //     'https://swapi.dev/api/films',
+  //   );
+  //   console.log(a);
+  //   return a.data;
+  // }
+
+  async findAll(): Promise<any> {
+    const { data } = await firstValueFrom(
+      this.httpService.get<any>('https://swapi.dev/api/films').pipe(
+        catchError((error: AxiosError) => {
+          this.logger.error(error.response.data);
+          throw 'An error happened!';
+        }),
+      ),
+    );
+    return data;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} movie`;
+  }
+
+  update(id: number, updateMovieDto: UpdateMovieDto) {
+    return `This action updates a #${id} movie`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} movie`;
+  }
+}
