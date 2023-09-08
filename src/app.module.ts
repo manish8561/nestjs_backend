b894a5cad74fe5ec48ae2ab019a74ad52edbf1d2
manish <manish.sharma@antechindia.com>
2023-09-08 19:14:28 +0530

    running all 3 container together with db, backend, frontend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { join } from 'path';
 import { AppController } from './app.controller';
@@ -12,7 +12,18 @@ import { ServeStaticModule } from '@nestjs/serve-static';
   imports: [
     ConfigModule.forRoot(),
     // MongooseModule.forRoot('mongodb://172.16.15.7:27017/nest'),
-    MongooseModule.forRoot('mongodb://mongodb:27017/nest'),
+    // `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongodb:27017/nest?authSource=admin`,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: `mongodb://${configService.get(
+          'MONGODB_USERNAME',
+        )}:${configService.get('MONGODB_PASSWORD')}@${configService.get(
+          'MONGODB_HOST',
+        )}:27017/nest?authSource=admin`,
+      }),
+      inject: [ConfigService],
+    }),
     FavoritesModule,
     MoviesModule,
     ServeStaticModule.forRoot({
